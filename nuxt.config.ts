96f61cac2d4c3d0dc3201d48f20bae27fec9a4f3
hazem hamdy <hh4morsy@gmail.com>
2024-08-21 16:13:19 +0300

    update ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,43 +1,50 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-const Title = "قيدها اكثر من مجرد حاسبة"
+const Title = "قيدها اكثر من مجرد حاسبة";
 const Description = `مكنك الآن عبر تطبيق "قيدها" إدارة جميع عمليات حساب نقاط البلوت وإدارة الجولات بشكل تلقائي تمامًا في
                 جميع الباقات الخاصة بتطبيق "قيدها". كما تتوفر خدمات خاصة في الباقات المميزة. اشترك الآن واحصل على هذه
                 المميزات الاستثنائية!
-`
-const Keywords = "زات , سام , بلوت , ورقة , لعب ,قيدها ,بلوت ,هاند , تريكس , كنكان ,تسجيل متقدم , حكام , شات , اوساسيونا , الادهم , طويق , دوري , الحزام , كاس , سوبر , اوراق , zat , baloot , sam , calculator"
-const LogoURL = "https://storage.googleapis.com/qydha_bucket/qydha_assets/qydha.com-assets/qydha-logo.svg"
-const WebsiteUrl = "https://qydha.com/"
+`;
+const Keywords =
+  "زات , سام , بلوت , ورقة , لعب ,قيدها ,بلوت ,هاند , تريكس , كنكان ,تسجيل متقدم , حكام , شات , اوساسيونا , الادهم , طويق , دوري , الحزام , كاس , سوبر , اوراق , zat , baloot , sam , calculator";
+const LogoURL =
+  "https://storage.googleapis.com/qydha_bucket/qydha_assets/qydha.com-assets/qydha-logo.svg";
+const WebsiteUrl = "https://qydha.com/";
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxt/ui','@pinia/nuxt'],
+  modules: ["@nuxt/ui", "@pinia/nuxt"],
   css: ["~/assets/css/main.css"],
 
   app: {
     head: {
       htmlAttrs: {
-        lang: 'ar',
-        dir: "rtl"
+        lang: "ar",
+        dir: "rtl",
       },
       title: Title,
       meta: [
-         { name: 'image' , property : 'og:image', content: LogoURL }
-         , { name: 'og:title', content: Title }
-         , { name: 'og:description', content: Description }
-         , { name: 'og:image', content: LogoURL }
-         , { name: 'og:url', content: WebsiteUrl }
-         , { name: 'author', content: "Zat Compony" }
-         , { hid: 'description', name: 'description', content: Description }
-        , { hid: 'keywords', name: 'keywords', content: Keywords }
-        , { "name": "viewport", "content": "width=device-width, initial-scale=1" }
-        , { "charset": "utf-8" }],
-      noscript: [{
-        children: `
+        { name: "image", property: "og:image", content: LogoURL },
+        { name: "og:title", content: Title },
+        { name: "og:description", content: Description },
+        { name: "og:image", content: LogoURL },
+        { name: "og:url", content: WebsiteUrl },
+        { name: "author", content: "Zat Compony" },
+        { hid: "description", name: "description", content: Description },
+        { hid: "keywords", name: "keywords", content: Keywords },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { charset: "utf-8" },
+      ],
+      noscript: [
+        {
+          children: `
           <p style='text-align: center;color:white ; background-color:rgb(218, 55, 55); margin:5px 10px; padding: 10px 5px ;  border-radius:10px '>Warning: Please Enable Js</p>
-        ` }],
-    }
+        `,
+        },
+      ],
+    },
   },
   colorMode: {
-    preference: 'light'
+    preference: "light",
   },
-  compatibilityDate: "2024-08-20"
+  compatibilityDate: "2024-08-20",
+  ssr: false,
 });
