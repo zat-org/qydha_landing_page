543ee100bf1c4253e9bcd8a769b2c538cd46bcb1
hazem hamdy <hh4morsy@gmail.com>
2024-08-21 16:11:33 +0300

    uopdate for global compoenent

--- app.vue
+++ app.vue
@@ -1,13 +1,14 @@
 <template>
   <div dir="rtl" lang="ar" class="flex flex-col min-h-screen bg-gray-100 text-[#222751] overflow-hidden">
-    <Header></Header>
+    <Header/>
     <main class="grow w-full relative">
       <NuxtPage />
     </main>
-    <Footer></Footer>
+    <Footer/>
   </div>
 </template>
 <script setup lang="ts">
-
+import Header from './components/global/Header.vue';
+import Footer from './components/global/Footer.vue';
 </script>
 <style></style>
