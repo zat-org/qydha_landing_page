795f28e59bd0c647d867c6ff4612d53dfb014990
hazem hamdy <hh4morsy$gmail.com>
2024-11-11 09:36:39 +0200

    update pagination

--- components/User/index.vue
+++ components/User/index.vue
@@ -1,15 +1,15 @@
 <template>
   <UCard :ui="{base:'flex flex-col h-full ',body:{base:'grow flex flex-col'}}">
     <template #header>
-      <div class="flex justify-between ">
+      <div class="flex justify-between items-center gap-2 ">
         <UInput v-model="query" placeholder="username or id  or phone " class="grow" />
-        <p> {{usersNumebr}} مستخدم </p>
+        <p> {{usersNumebr?.toLocaleString()}} مستخدم </p>
       </div>
 
     </template>
 
       <UTable :rows="rows" :columns="cols" class="grow " :ui="{td:{padding:'py-1 sm:py-1'},th:{padding:'py-1 sm:py-1'}}" @select="select" :loading="usersREQ.status.value=='pending'"></UTable>
-      <UPagination v-model="page" :page-count="5" :total="items"  class="mx-auto"/>
+      <UPagination v-model="page" :page-count="10" :total="items"  class="mx-auto"/>
 
   </UCard>
 </template>
@@ -43,6 +43,7 @@ const select=(row:User)=>{
 watch(query ,async (newValue,oldValue)=>{
 await usersREQ.fetchREQ(newValue)
 if (usersREQ.status.value=="success"){
+
   page.value = usersREQ.data.value?.data.currentPage!
   items.value = usersREQ.data.value?.data.totalCount!
 }  
