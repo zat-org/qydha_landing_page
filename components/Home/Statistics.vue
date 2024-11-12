42ee63efe768d634ced5fa77f018fe11613fbcb3
hazem hamdy <hh4morsy$gmail.com>
2024-11-12 08:19:54 +0200

    update statics

--- components/Home/Statistics.vue
+++ components/Home/Statistics.vue
@@ -3,11 +3,11 @@
         class=" bg-[#1d2145] text-white flex justify-around py-5 rounded-lg relative overflow-hidden">
         <img src="@/assets/images/circle.svg" alt="circle" class="absolute -left-5 top-1/2 -translate-y-1/2" />
         <div class="text-center z-10">
-            <p class="text-2xl mb-2">35,000 +</p>
+            <p class="text-2xl mb-2">45,000 +</p>
             <h3>عدد التنزيلات</h3>
         </div>
         <div class="text-center z-10">
-            <p class="text-2xl mb-2">8,000 +</p>
+            <p class="text-2xl mb-2">10,000 +</p>
             <h3>عدد المستخدمين</h3>
         </div>
         <img src="@/assets/images/circle.svg" alt="circle" class="absolute  -right-5 top-1/2 -translate-y-1/2" />
